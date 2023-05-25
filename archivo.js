2d91aa8b58e70a0f23fda4158d72b96aa9e4781b
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-25 20:11:39 -0300

    improving the code testing tijeraButton add event listener

--- archivo.js
+++ archivo.js
@@ -1,7 +1,9 @@
 const piedraButton = document.getElementById("piedra")
 const tijeraButton = document.getElementById("tijera")
 const papelButton = document.getElementById("papel")
-
+const icon =  document.getElementById("icon")
+const p = document.getElementById("p")
+const resultBox = document.getElementById("result")
 
 let generarRespuesta = function () {
     const numero = Math.random()
@@ -15,3 +17,36 @@ let generarRespuesta = function () {
         return "Papel"
     }
 }
+
+
+tijeraButton.addEventListener("click", function(){
+    const resultado = generarRespuesta()
+    console.log(resultado);
+    if(resultado === "Piedra"){
+      resultBox.classList.remove("none")
+      p.classList.add("lose")
+      icon.classList.add("lose")
+      icon.classList.add("fa-solid fa-thumbs-down")
+  
+      p.innerHTML = "Has perdido, la maquina jugo Piedra y tu has jugado tijera"
+  }
+  
+  else if(resultado === "Tijera"){
+      resultBox.classList.remove("none")
+      p.classList.add("draw")
+      icon.classList.add("draw")
+      icon.classList.add("fa-solid fa-handshake")
+      
+      p.innerHTML = "Has empatado, la maquina jugo Tijera y tu has jugado Tijera"
+  }else{
+      resultBox.classList.remove("none")
+      p.classList.add("win")
+      icon.classList.add("win")
+      icon.classList.add("fa-sharp fa-solid fa-thumbs-up")
+      
+      p.innerHTML = "Has ganado, la maquina jugo Papel y tu has jugado Tijera"
+  }
+  })
+
+
+
