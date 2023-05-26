878c3155074ef5baff3fb3e20fbc97553ac626a2
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-26 19:41:02 -0300

    adding image to the machine response

--- archivo.js
+++ archivo.js
@@ -1,28 +1,17 @@
 const piedraButton = document.getElementById("piedra")
 const tijeraButton = document.getElementById("tijera")
 const papelButton = document.getElementById("papel")
-const icon =  document.getElementById("icon")
+const img =  document.getElementById("img")
 const p = document.getElementById("p")
 const resultBox = document.getElementById("result")
 
-let generarRespuesta = function () {
-    const numero = Math.random()
 
-
-    if(numero >= 0.66){
-        return "Tijera"
-    }else if(numero < 0.66 && numero >= 0.33 ){
-        return "Piedra"
-    }else{
-        return "Papel"
-    }
-}
 
 
 tijeraButton.addEventListener("click", function(){
     const numero = Math.random()
 let resultado;
-
+console.log(img);
     if(numero >= 0.66){
         resultado = "Tijera"
     }else if(numero < 0.66 && numero >= 0.33 ){
@@ -36,10 +25,10 @@ let resultado;
       p.classList.add("lose")
       p.classList.remove("win")
       p.classList.remove("draw")
-      icon.classList.add("lose")
+    
      // icon.classList.add("fa-solid fa-thumbs-down")
-  
-      p.innerHTML = "Has perdido, la maquina jugo Piedra y tu has jugado tijera"
+     img.src = "./perdiste.jpg"
+      p.innerHTML = "Has perdido, la maquina jugo Piedra y tu has jugado Tijera."
       console.log("perdiste");
   }
   
@@ -48,10 +37,10 @@ let resultado;
       p.classList.add("draw")
       p.classList.remove("win")
       p.classList.remove("lose")
-      icon.classList.add("draw")
+      
     //  icon.classList.add("fa-solid fa-handshake")
       
-      p.innerHTML = "Has empatado, la maquina jugo Tijera y tu has jugado Tijera"
+      p.innerHTML = "Has empatado, la maquina jugo Tijera y tu has jugado Tijera."
 
       console.log("empataste");
 
@@ -60,10 +49,10 @@ let resultado;
       p.classList.add("win")
       p.classList.remove("lose")
       p.classList.remove("draw")
-      icon.classList.add("win")
+
     //  icon.classList.add("fa-sharp fa-solid fa-thumbs-up")
       
-      p.innerHTML = "Has ganado, la maquina jugo Papel y tu has jugado Tijera"
+      p.innerHTML = "Has ganado, la maquina jugo Papel y tu has jugado Tijera."
 
       console.log("ganaste");
 
