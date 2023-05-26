6cc40fd0d1af1350eac51b78fce50b6a89745467
Mateo Quotteri <quotterimateo@gmail.com>
2023-05-26 08:23:40 -0300

    the response is working (the code)

--- archivo.js
+++ archivo.js
@@ -20,31 +20,53 @@ let generarRespuesta = function () {
 
 
 tijeraButton.addEventListener("click", function(){
-    const resultado = generarRespuesta()
-    console.log(resultado);
+    const numero = Math.random()
+let resultado;
+
+    if(numero >= 0.66){
+        resultado = "Tijera"
+    }else if(numero < 0.66 && numero >= 0.33 ){
+        resultado = "Piedra"
+    }else{
+        resultado = "Papel"
+    }
+
     if(resultado === "Piedra"){
       resultBox.classList.remove("none")
       p.classList.add("lose")
+      p.classList.remove("win")
+      p.classList.remove("draw")
       icon.classList.add("lose")
-      icon.classList.add("fa-solid fa-thumbs-down")
+     // icon.classList.add("fa-solid fa-thumbs-down")
   
       p.innerHTML = "Has perdido, la maquina jugo Piedra y tu has jugado tijera"
+      console.log("perdiste");
   }
   
   else if(resultado === "Tijera"){
       resultBox.classList.remove("none")
       p.classList.add("draw")
+      p.classList.remove("win")
+      p.classList.remove("lose")
       icon.classList.add("draw")
-      icon.classList.add("fa-solid fa-handshake")
+    //  icon.classList.add("fa-solid fa-handshake")
       
       p.innerHTML = "Has empatado, la maquina jugo Tijera y tu has jugado Tijera"
+
+      console.log("empataste");
+
   }else{
       resultBox.classList.remove("none")
       p.classList.add("win")
+      p.classList.remove("lose")
+      p.classList.remove("draw")
       icon.classList.add("win")
-      icon.classList.add("fa-sharp fa-solid fa-thumbs-up")
+    //  icon.classList.add("fa-sharp fa-solid fa-thumbs-up")
       
       p.innerHTML = "Has ganado, la maquina jugo Papel y tu has jugado Tijera"
+
+      console.log("ganaste");
+
   }
   })
 
